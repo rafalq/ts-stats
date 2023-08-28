bd162cad206d91c814e526418e31b7c2d877ac77
rafalq <kruszewskirafael@gmail.com>
2023-08-28 09:55:23 +0200

    describe data row with tuple

--- src/classes/CsvFileReader.ts
+++ src/classes/CsvFileReader.ts
@@ -2,20 +2,22 @@ import fs from 'fs';
 import { dateStringtoDate } from '../utils';
 import { MatchResult } from '../MatchResults';
 
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
 
   read(): void {
-    fs.readFileSync(this.filename, {
-      encoding: 'utf-8',
-    })
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: 'utf-8',
+      })
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map((row: string[]): any => {
+      .map((row: string[]): MatchData => {
         return [
           dateStringtoDate(row[0]),
           row[1],
