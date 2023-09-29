e4ba1cbf11e9c7c65a345fc4227dd5482dea78ab
rafalq <kruszewskirafael@gmail.com>
2023-09-29 11:32:22 +0200

    create WinsAnalysis class

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,3 @@ const csvFileReader = new CsvFileReader('football.csv');
 // create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
